600ddd5baadd05c96f81082880b9c549dca76e94
Aasim040 <aasim040@gmail.com>
2022-02-10 15:54:03 -0500

    Changed where the score counter was

--- Assets/01-Apple Picker/HighScore.cs
+++ Assets/01-Apple Picker/HighScore.cs
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +26,10 @@ public class HighScore : MonoBehaviour
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
