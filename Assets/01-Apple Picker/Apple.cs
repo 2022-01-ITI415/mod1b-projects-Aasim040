577a93602c15822920e23c71f663cd827862a480
Aasim040 <aasim040@gmail.com>
2022-02-10 13:23:24 -0500

    Added scripts to the apple, Apple Tree, and added baskets into the game

--- Assets/01-Apple Picker/Apple.cs
+++ Assets/01-Apple Picker/Apple.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    public static float bottomY = -20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/01-Apple Picker/ApplePicker.cs
+++ Assets/01-Apple Picker/ApplePicker.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
