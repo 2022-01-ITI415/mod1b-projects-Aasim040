28d275342e925a1ff6b83542ea25cd3175ed7380
Aasim040 <aasim040@gmail.com>
2022-02-10 14:14:42 -0500

    Allowed the apples to collide with the basket

--- Assets/01-Apple Picker/Basket.cs
+++ Assets/01-Apple Picker/Basket.cs
@@ -7,12 +7,28 @@ public class Basket : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple") ;
+        {
+            Destroy(collidedWith);
+        }
+    }
+
 }
