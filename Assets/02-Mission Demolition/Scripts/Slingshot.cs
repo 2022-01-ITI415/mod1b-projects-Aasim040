e00fe89b30fb70ae2bb9fda4717cb424532d264e
Aasim040 <aasim040@gmail.com>
2022-02-16 21:19:17 -0500

    Added a Halo to the Slingshot and wrote code to make it turn on and off

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint"); //a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false); //b
+
+    }
     void OnMouseEnter()
     {
-        print("Slingshot: OnMouseEnter()");
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true); //b
     }
 
     void OnMouseExit()
     {
-        print("Slingshot: OnMouseExit()");
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false); //b
     }
     // Start is called before the first frame update
     void Start()
