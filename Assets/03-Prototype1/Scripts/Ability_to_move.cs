f4312f2fc13a31b3b954fca6a17fd5e5eee8732e
Aasim040 <aasim040@gmail.com>
2022-02-21 13:53:05 -0500

    Added in Win and Count Text. and made all the pickups collectible

--- Assets/03-Prototype1/Scripts/Ability_to_move.cs
+++ Assets/03-Prototype1/Scripts/Ability_to_move.cs
@@ -5,15 +5,20 @@ using UnityEngine.UI;
 
 public class Ability_to_move : MonoBehaviour
 {
+
     public float speed;
-
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
+    private int count;
 
-void Start() {
-
-    rb = GetComponent<Rigidbody>();
-}
-
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
     void FixedUpdate()
     {
 
@@ -25,4 +30,28 @@ void Start() {
         rb.AddForce(movement * speed);
 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
 }
